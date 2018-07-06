0aaa30f9e88c9db58122b0c9cc47b40629c5bbaa
Uy Binh <uybinh@gmail.com>
2018-07-06 22:36:59 +0700

    Edit users routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_digest)
+    end
 end
