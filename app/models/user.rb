9b74b96ce0b2c34c11a454a70a891dd32bb74198
Uy Binh <uybinh@gmail.com>
2018-07-07 20:14:55 +0700

    Add form partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
                     length: {maximum: 50}
   validates :email, presence: true,
                     format: {with: EMAIL_REGEX},
+                    uniqueness: true,
                     length: {maximum: 255}
   validates :password, presence: true,
                       length: {minimum: 6},
