0324d5fad17098236aee79a0da808ef71afbea05
Uy Binh <uybinh@gmail.com>
2018-07-07 23:19:03 +0700

    Add login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users
-  get "/login", to: "sessions#new"
   get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
 end
