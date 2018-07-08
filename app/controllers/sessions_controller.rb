56723df4e9716ff760fd7915ea92880f54b6b65a
Uy Binh <uybinh@gmail.com>
2018-07-08 09:46:58 +0700

    Add logout button
    add logout button, session destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,10 @@ class SessionsController < ApplicationController
       flash.now[:danger] = "Invalid email/password combination"
       render :new
     end
+  end
 
+  def destroy
+    
   end
   
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,9 +3,26 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def logged_in?
+    !current_user.nil?
+  end
+
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+
+  def current_user
+    @current_user ||= session_user || cookie_user
+  end
+
+  def session_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def cookie_user
+    User.find_by(id: cookies.permanent.signed[:user_id])
+  end
+
 end
