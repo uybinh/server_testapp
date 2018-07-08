6096935857fd063e9c4a84518f07ed0eb752e19d
Uy Binh <uybinh@gmail.com>
2018-07-08 09:09:41 +0700

    Add log_ing function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      remember(@user)
       redirect_to @user
     else
       flash.now[:danger] = "Invalid email/password combination"
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,11 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
 end
